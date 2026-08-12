5d36ed83139575200ba81fbe5d8d11f3cb6de8d1
Saswat Pal <saswat2001@gmail.com>
2026-08-12 16:16:49 +0530

    initial setup and add diagrams

---
+++ src/test/java/com/saswat/lovable/LovableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saswat.lovable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LovableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
